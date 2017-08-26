69c6b97bb98c870885d4d7d3195020e724f84eaa
doug <doug.shawhan@gmail.com>
2017-08-26 02:30:19 -0400

    trying to please pypi. failing

--- setup.py
+++ setup.py
@@ -3,9 +3,19 @@
 from distutils.core import setup
 
 setup(name="JSONAutoArray",
-      version="0.1",
-      description="conveniently write json-serializable python objects to an array in a file",
-      author="doug shawhan",
-      author_email="doug.shawhan@gmail.com",
-      packages=["json_autoarray"],
+    version="0.1",
+    description="conveniently write json-serializable python objects to an array in a file",
+    author="doug shawhan",
+    author_email="doug.shawhan@gmail.com",
+    url="neuralpro.be",
+    packages=["json_autoarray"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: ETL",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        ],
+    python_requires=">=2.6"
      )
