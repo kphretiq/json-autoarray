83a56b4d44b9352f3dbe02bdf0b5fc968975167e
doug <doug.shawhan@gmail.com>
2017-08-26 15:24:59 -0400

    tidied up a bit

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@
 from setuptools import setup
 
 setup(name="JSONAutoArray",
-    version="0.3",
-    description="conveniently write json-serializable python objects to an array in a file",
+    version="0.5",
+    description="Write self-closing array to JSON file",
+    long_description="conveniently write json-serializable python objects to an array in a file",
     author="pathetiq kphretiq",
     author_email="kphretiq@gmail.com",
+    maintainer="pathetiq kphretiq",
+    maintainer_email="kphretiq@gmail.com",
     url="https://github.com/kphretiq/json-autoarray",
+    download_url="https://github.com/kphretiq/json-autoarray",
     packages=["json_autoarray"],
     classifiers=[
         "Intended Audience :: Developers",
